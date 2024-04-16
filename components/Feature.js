f25a3248ffbb96270c5535be1c73597a5ec9e4c7
Emmanuel Soriano <emmso735@gmail.com>
2024-04-16 19:32:34 -0400

    Update Feature.js
    Fixing the feature component

--- components/Feature.js
+++ components/Feature.js
@@ -1,35 +1,32 @@
-import Center from "@/components/Center";
-import styled from "styled-components";
-import ButtonLink from "@/components/ButtonLink";
-import CartIcon from "@/components/icons/Cart";
-import FlyingButton from "@/components/FlyingButton";
-import {RevealWrapper} from 'next-reveal'
+import Center from '@/components/Center';
+import styled from 'styled-components';
+import ButtonLink from '@/components/ButtonLink';
+import CartIcon from '@/components/icons/Cart';
+import FlyingButton from '@/components/FlyingButton';
+import { RevealWrapper } from 'next-reveal';
 
 const Bg = styled.div`
-    background-color: #222;
-    color: #fff;
-    padding: 50px 0;
+  background-color: #222;
+  color: #fff;
+  padding: 50px 0;
 `;
-
 const Title = styled.h1`
-    margin: 0;
-    font-weight: normal;
-    font-size: 2rem;
-    @media screen and (min-width: 768px) {
-        font-size: 2.5rem;
-    }
+  margin: 0;
+  font-weight: normal;
+  font-size: 1.5rem;
+  @media screen and (min-width: 768px) {
+    font-size: 3rem;
+  }
 `;
-
 const Desc = styled.p`
-    color: #aaa;
-    font-size: 1.2rem;
+  color: #aaa;
+  font-size: 0.8rem;
 `;
-
 const ColumnsWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   gap: 40px;
-  img.main{
+  img.main {
     max-width: 100%;
     max-height: 200px;
     display: block;
@@ -45,71 +42,76 @@ const ColumnsWrapper = styled.div`
     & > div:nth-child(1) {
       order: 0;
     }
-    img{
+    img {
       max-width: 100%;
     }
   }
 `;
-
 const Column = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
-
 const ButtonsWrapper = styled.div`
-    display: flex;
-    gap: 10px;
-    margin-top: 25px;
+  display: flex;
+  gap: 10px;
+  margin-top: 25px;
 `;
-
 const CenterImg = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 `;
 
 const ImgColumn = styled(Column)`
-    & > div{
-        width: 100%
-    }
+  & > div {
+    width: 100%;
+  }
 `;
 
-const ContentWrapper = styled.div`
+const ContentWrapper = styled.div``;
 
-`;
-
-export default function Feature({product}) {
-{product && (
-        <Bg>
-            <Center>
-                <ColumnsWrapper>
-                    <Column>
-                        <div>
-                            <RevealWrapper origin={'left'} delay={0}>
-                                <ContentWrapper>
-                                    <Title>{product.title}</Title>
-                                    <Desc>{product.description}</Desc>
-                                    <ButtonsWrapper>
-                                        <ButtonLink href={'/product/'+product._id} outline={1} white={1} >Read more</ButtonLink>
-                                        <FlyingButton white={1} _id={product._id} src={product.images?.[0]}>
-                                            <CartIcon />
-                                            Add to cart
-                                        </FlyingButton>
-                                    </ButtonsWrapper>
-                                </ContentWrapper>
-                            </RevealWrapper>
-                        </div>
-                    </Column>
-                    <ImgColumn>
-                    <RevealWrapper delay={0}>
-                        <CenterImg>
-                            <img className={'main'} src={product.images?.[0]} alt=""/>
-                        </CenterImg>
-                    </RevealWrapper>
-                    </ImgColumn>
-                </ColumnsWrapper>
-            </Center>
-        </Bg>
-    )}
+export default function Featured({ product }) {
+  return (
+    <Bg>
+      <Center>
+        <ColumnsWrapper>
+          <Column>
+            <div>
+              <RevealWrapper origin={'left'} delay={0}>
+                <ContentWrapper>
+                  <Title>{product.title}</Title>
+                  <Desc>{product.description}</Desc>
+                  <ButtonsWrapper>
+                    <ButtonLink
+                      href={'/product/' + product._id}
+                      outline={1}
+                      white={1}
+                    >
+                      Read more
+                    </ButtonLink>
+                    <FlyingButton
+                      white={1}
+                      _id={product._id}
+                      src={product.images?.[0]}
+                    >
+                      <CartIcon />
+                      Add to cart
+                    </FlyingButton>
+                  </ButtonsWrapper>
+                </ContentWrapper>
+              </RevealWrapper>
+            </div>
+          </Column>
+          <ImgColumn>
+            <RevealWrapper delay={0}>
+              <CenterImg>
+                <img className={'main'} src={product.images?.[0]} alt="" />
+              </CenterImg>
+            </RevealWrapper>
+          </ImgColumn>
+        </ColumnsWrapper>
+      </Center>
+    </Bg>
+  );
 }
