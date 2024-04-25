24d4c30d7f2cfdb174269361e91c9d1a5b6ff811
Emmanuel Soriano <emmso735@gmail.com>
2024-04-25 19:58:03 -0400

    changing the colors of the website

--- components/Feature.js
+++ components/Feature.js
@@ -4,23 +4,27 @@ import ButtonLink from '@/components/ButtonLink';
 import CartIcon from '@/components/icons/Cart';
 import FlyingButton from '@/components/FlyingButton';
 import { RevealWrapper } from 'next-reveal';
+import { colors } from '@/lib/colors';
 
 const Bg = styled.div`
-  background-color: #222;
-  color: #fff;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' width='100%' height='560' preserveAspectRatio='none' viewBox='0 0 1440 560'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1009%26quot%3b)' fill='none'%3e%3crect width='1440' height='560' x='0' y='0' fill='rgba(119%2c 52%2c 231%2c 1)'%3e%3c/rect%3e%3cpath d='M 0%2c281 C 57.6%2c264.4 172.8%2c148.6 288%2c198 C 403.2%2c247.4 460.8%2c530.2 576%2c528 C 691.2%2c525.8 748.8%2c235.4 864%2c187 C 979.2%2c138.6 1036.8%2c295.2 1152%2c286 C 1267.2%2c276.8 1382.4%2c170 1440%2c141L1440 560L0 560z' fill='rgba(205%2c 121%2c 245%2c 1)'%3e%3c/path%3e%3cpath d='M 0%2c451 C 96%2c363 288%2c22.8 480%2c11 C 672%2c-0.8 768%2c353.4 960%2c392 C 1152%2c430.6 1344%2c241.6 1440%2c204L1440 560L0 560z' fill='rgba(255%2c 254%2c 255%2c 1)'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1009'%3e%3crect width='1440' height='560' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");
+  color: ${colors.primary};
   padding: 50px 0;
 `;
 const Title = styled.h1`
   margin: 0;
   font-weight: normal;
+  color: ${colors.text};
+  text-shadow: 3px 3px 5px ${colors.secondary}, 6px 6px 5px ${colors.title},
+    1px 1px 5px ${colors.text};
   font-size: 1.5rem;
   @media screen and (min-width: 768px) {
     font-size: 3rem;
   }
 `;
 const Desc = styled.p`
-  color: #aaa;
-  font-size: 0.8rem;
+  color: ${colors.primary};
+  font-size: 1.2rem;
 `;
 const ColumnsWrapper = styled.div`
   display: grid;
@@ -80,18 +84,20 @@ export default function Featured({ product }) {
             <div>
               <RevealWrapper origin={'left'} delay={0}>
                 <ContentWrapper>
-                  <Title>{product.title}</Title>
+                  <Title>
+                    <h2>{product.title}</h2>
+                  </Title>
                   <Desc>{product.description}</Desc>
                   <ButtonsWrapper>
                     <ButtonLink
                       href={'/product/' + product._id}
                       outline={1}
-                      white={1}
+                      primary={1}
                     >
                       Read more
                     </ButtonLink>
                     <FlyingButton
-                      white={1}
+                      primary={1}
                       _id={product._id}
                       src={product.images?.[0]}
                     >
@@ -115,3 +121,5 @@ export default function Featured({ product }) {
     </Bg>
   );
 }
+
+//background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' width='100%' height='560' preserveAspectRatio='none' viewBox='0 0 1440 560'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1009%26quot%3b)' fill='none'%3e%3crect width='1440' height='560' x='0' y='0' fill='rgba(119%2c 52%2c 231%2c 1)'%3e%3c/rect%3e%3cpath d='M 0%2c281 C 57.6%2c264.4 172.8%2c148.6 288%2c198 C 403.2%2c247.4 460.8%2c530.2 576%2c528 C 691.2%2c525.8 748.8%2c235.4 864%2c187 C 979.2%2c138.6 1036.8%2c295.2 1152%2c286 C 1267.2%2c276.8 1382.4%2c170 1440%2c141L1440 560L0 560z' fill='rgba(205%2c 121%2c 245%2c 1)'%3e%3c/path%3e%3cpath d='M 0%2c451 C 96%2c363 288%2c22.8 480%2c11 C 672%2c-0.8 768%2c353.4 960%2c392 C 1152%2c430.6 1344%2c241.6 1440%2c204L1440 560L0 560z' fill='rgba(255%2c 254%2c 255%2c 1)'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1009'%3e%3crect width='1440' height='560' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");
--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,22 @@
-import Link from "next/link";
-import styled from "styled-components";
-import Center from "@/components/Center";
-import { useContext, useState } from "react";
-import { CartContext } from "@/components/CartContext";
-import BarsIcon from "@/components/icons/Bars"
-import SearchIcon from "@/components/icons/SearchIcon"
+import Link from 'next/link';
+import styled from 'styled-components';
+import Center from '@/components/Center';
+import { colors } from '@/lib/colors';
+import { useContext, useState } from 'react';
+import { CartContext } from '@/components/CartContext';
+import BarsIcon from '@/components/icons/Bars';
+import SearchIcon from '@/components/icons/SearchIcon';
 
 const StyledHeader = styled.header`
-  background-color: #222;
+  background-color: ${colors.secondary};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
 const Logo = styled(Link)`
-  color:#fff;
-  text-decoration:none;
+  color: ${colors.text};
+  text-decoration: none;
   position: relative;
   z-index: 3;
 `;
@@ -27,9 +28,12 @@ const Wrapper = styled.div`
 `;
 
 const StyledNav = styled.nav`
-  ${props => props.mobileNavActive ? `
+  ${(props) =>
+    props.mobileNavActive
+      ? `
     display: block;
-  ` : `
+  `
+      : `
     display: none;
   `}
   gap: 15px;
@@ -39,7 +43,6 @@ const StyledNav = styled.nav`
   left: 0;
   right: 0;
   padding: 70px 20px 20px;
-  background-color: #222;
   @media screen and (min-width: 768px) {
     display: flex;
     position: static;
@@ -48,25 +51,25 @@ const StyledNav = styled.nav`
 `;
 
 const NavLink = styled(Link)`
-    display: block;
-    color:#aaa;
-    text-decoration:none;
-    min-width: 30px;
-    padding: 10px 0;
-    svg{
-      height: 20px
-    }
-    @media screen and (min-width: 768px) {
-        padding:0;
-    }
+  display: block;
+  color: ${colors.text};
+  text-decoration: none;
+  min-width: 30px;
+  padding: 10px 0;
+  svg {
+    height: 20px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 0;
+  }
 `;
 
 const NavButton = styled.button`
   background-color: transparent;
   width: 30px;
   height: 30px;
-  border:0;
-  color: white;
+  border: 0;
+  color: black;
   cursor: pointer;
   position: relative;
   z-index: 3;
@@ -78,11 +81,11 @@ const NavButton = styled.button`
 const SideIcons = styled.div`
   display: flex;
   align-items: center;
-  a{
+  a {
     display: inline-block;
     min-width: 20px;
     color: white;
-    svg{
+    svg {
       width: 14px;
       height: 14px;
     }
@@ -90,35 +93,38 @@ const SideIcons = styled.div`
 `;
 
 const Name = styled.span`
-  color: #6B8E23;
+  color: ${colors.title};
 `;
 
 export default function Header() {
-    const {cartProducts} = useContext(CartContext);
-    const [mobileNavActive,setMobileNavActive] = useState(false);
-    return (
-        <StyledHeader>
-            <Center>
-                <Wrapper>
-                    <Logo href={'/'}>
-                    <Name>Hey</Name>
-                        Lisen
-                      </Logo>   {/*componente Link */}
-                    <StyledNav mobileNavActive={mobileNavActive}>
-                        <NavLink href={'/'}>Home</NavLink>
-                        <NavLink href={'/products'}>All products</NavLink>
-                        <NavLink href={'/categories'}>Categories</NavLink>
-                        <NavLink href={'/account'}>Account</NavLink>
-                        <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
-                    </StyledNav>
-                    <SideIcons>
-                      <Link href={'/search'}><SearchIcon/></Link>
-                      <NavButton onClick={() => setMobileNavActive(prev => !prev)}>
-                          <BarsIcon/>
-                      </NavButton>
-                    </SideIcons>
-                </Wrapper>
-            </Center>
-        </StyledHeader>
-    )
+  const { cartProducts } = useContext(CartContext);
+  const [mobileNavActive, setMobileNavActive] = useState(false);
+  return (
+    <StyledHeader>
+      <Center>
+        <Wrapper>
+          <Logo href={'/'}>
+            <Name>Hey</Name>
+            Lisen
+          </Logo>{' '}
+          {/*componente Link */}
+          <StyledNav mobileNavActive={mobileNavActive}>
+            <NavLink href={'/'}>Home</NavLink>
+            <NavLink href={'/products'}>All products</NavLink>
+            <NavLink href={'/categories'}>Categories</NavLink>
+            <NavLink href={'/account'}>Account</NavLink>
+            <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
+          </StyledNav>
+          <SideIcons>
+            <Link href={'/search'}>
+              <SearchIcon />
+            </Link>
+            <NavButton onClick={() => setMobileNavActive((prev) => !prev)}>
+              <BarsIcon />
+            </NavButton>
+          </SideIcons>
+        </Wrapper>
+      </Center>
+    </StyledHeader>
+  );
 }
